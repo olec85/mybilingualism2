bba9e55933b0e486e8fb5ebdce6637766721f4b0
olec85 <olec85@bk.ru>
2024-02-26 16:07:57 +0300

    ///

---
+++ script.js
@@ -0,0 +1,61 @@
+document.getElementById('arrowDown').addEventListener('click', ()=> {
+    document.getElementById('burger').classList.toggle('hidden')
+})
+
+document.getElementById('arrowDown1').addEventListener('click', ()=> {
+    document.getElementById('burger').classList.toggle('hidden')
+})
+
+document.getElementById('arrowDown2').addEventListener('click', ()=> {
+    document.getElementById('burger').classList.toggle('hidden')
+})
+
+document.getElementById('heartDown').addEventListener('click', ()=> {
+    document.getElementById('burger').classList.toggle('hidden')
+})
+
+document.getElementById('heartDown').addEventListener('click', ()=> {
+    document.getElementById('burger').classList.toggle('hidden')
+})
+
+
+const images = document.querySelectorAll('.slider .slider-line-item');
+const sliderLine = document.querySelector('.slider-line');
+
+let count = 0;
+let width;
+
+function init() {
+    console.log('resize');
+    width = document.querySelector('.slider').offsetWidth;
+    sliderLine.style.width = width * images.length + 'px';
+    images.forEach( item => {
+        item.style.width = width + 'px';
+        item.style.height  = 'auto';
+    });
+    rollSlider();
+}
+
+window.addEventListener('resize', init);
+
+init();
+
+document.querySelector('.slider-prev').addEventListener('click', function() {
+    count--;
+    if (count < 0) {
+        count = images.length - 1;
+    }
+    rollSlider();
+});
+
+document.querySelector('.slider-next').addEventListener('click', function() {
+    count++;
+    if (count >= images.length) {
+        count = 0;
+    }
+    rollSlider();
+});
+
+function rollSlider() {
+    sliderLine.style.transform = 'translate(-'+count*width+'px)';
+}
